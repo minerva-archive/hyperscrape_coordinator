bfce96489b16174f603d778f4fd4047d7f68002c
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-03-08 09:35:05 +0000

    Remove Sqlite3

--- state_db.py
+++ state_db.py
@@ -1,248 +1,200 @@
-import queue
-from typing import Callable, Any
-import threading
-import sqlite3
+from psycopg import AsyncConnection
+import psycopg_pool
 
 class StateDB:
     """!
     @brief The state's database storage system
     """
 
+    def __init__(self, conninfo: str):
+        self._pool = psycopg_pool.AsyncConnectionPool(conninfo)
 
-    def __init__(self, db_path: str):
-        self._db_path = db_path
-
-        # we have multiple threads, so need to guarantee only one write at a time
-        # sqlite is not thread safe and this is simpler than a writer queue
-        # as of writing, it seems only main is writing the states during normal ops
-        # This is kinda slow but it should be fine for our use-case -HD
-        self._local = threading.local()
-        self._write_lock = threading.Lock()
-        self._write_conn = sqlite3.connect(db_path, check_same_thread=False)
-        self._configure(self._write_conn)
-        self._initialize_db()
-
-        # aaaaand the performance is not cutting it, still need to batch writes
-        self._write_queue = queue.Queue()
-
+    async def get_connection(self):
+        return await self._pool.connection()
     
-    def close(self):
-        self._write_conn.close()
+    async def close(self):
+        await self._pool.close()    
+
+class StateDBConnection:
+    def __init__(self, stateDB: StateDB):
+        self.connection: AsyncConnection = None
+        self.stateDB = stateDB
+
+    async def __enter__(self):
+        self.connection = await self.stateDB.get_connection()
+
+    async def __exit__(self):
+        self.connection.close()
+
+    # Get objects
+    async def get_files(self) -> list[dict]:
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT * FROM file")
+            return await cur.fetchall()
+        
+    async def get_file(self, file_id: str) -> dict:
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT * FROM file WHERE id = $1", (file_id,))
+            return await cur.fetchone()
+
+    async def get_chunks(self) -> list[dict]:
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT * FROM chunk")
+            return await cur.fetchall()
+        
+    async def get_chunk(self, chunk_id: str) -> dict:
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT * FROM chunk WHERE id = $1", (chunk_id,))
+            return await cur.fetchone()
+
+    async def get_chunks_for_file(self, file_id: str) -> list[dict]:
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT * FROM chunk WHERE file_id = $1", (file_id,))
+            return await cur.fetchall()
+
+    async def get_chunk_worker_status(self, chunk_id: str) -> list[dict]:
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT * FROM worker_status WHERE chunk_id = $1", (chunk_id,))
+            return await cur.fetchall()
+
+    async def get_file_hashes(self):
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT path, md5, sha1, sha256 FROM file_hash JOIN file on file.id = file_hash.file_id")
+            return await cur.fetchall()
+
+    async def get_leaderboard(self):
+        async with self.connection.cursor() as cur:
+            await cur.execute("SELECT * FROM leaderboard ORDER BY downloaded_bytes DESC")
+            return await cur.fetchall()
+
+    # File mutations
+    async def insert_file(self, file_id: str, path: str, size: int, url: str, chunk_size: int, complete: bool = False):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "INSERT INTO file (id, path, size, url, chunk_size, complete) "
+                "VALUES ($1, $2, $3, $4, $5, $6)",
+                (file_id, path, size, url, chunk_size, complete)
+            )
+
+    async def set_file_size(self, file_id: str, size: int):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE file SET size = $1 WHERE id = $2",
+                (size, file_id)
+            )
+
+    async def set_file_chunk_size(self, file_id: str, chunk_size: int):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE file SET chunk_size = $1 WHERE id = $2",
+                (chunk_size, file_id)
+            )
+
+    async def set_file_complete(self, file_id: str):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE file SET complete = TRUE WHERE id = $1",
+                (file_id,)
+            )
 
 
-    @property
-    def _conn(self):
-        # reading conn for each thread
-        if not hasattr(self._local, "conn"):
-            self._local.conn = sqlite3.connect(self._db_path)
-            self._configure(self._local.conn)
-        return self._local.conn
+    # Chunk mutations
+    async def insert_chunk(self, chunk_id: str, file_id: str, start: int, end: int):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "INSERT INTO chunk (id, file_id, start, end) "
+                "VALUES ($1, $2, $3, $4)",
+                (chunk_id, file_id, start, end)
+            )
 
+    async def delete_chunk(self, chunk_id: str):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "DELETE FROM chunk WHERE id = $1",
+                (chunk_id,)
+            )
 
-    @staticmethod
-    def _configure(conn: sqlite3.Connection):
-        conn.execute("PRAGMA foreign_keys = ON")
-        conn.execute("PRAGMA journal_mode = WAL")
-        conn.execute("PRAGMA synchronous = normal")
-        conn.execute("PRAGMA page_size = 8192")
-        conn.execute("PRAGMA journal_size_limit = 6144000")
-        conn.row_factory = sqlite3.Row
+    async def insert_worker_status(self, chunk_id: str, worker_id: str, uploaded: int = 0, hash: str = "", hash_only: bool = True):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "INSERT OR REPLACE INTO worker_status (chunk_id, worker_id, uploaded, hash, hash_only) "
+                "VALUES ($1, $2, $3, $4, $5)",
+                (chunk_id, worker_id, uploaded, hash, hash_only)
+            )
 
-
-    def _queue_write(self, query: str, params: tuple):
-        self._write_queue.put((query, params))
-
-
-    def flush(self):
-        ops = []
-        while True:
-            try:
-                ops.append(self._write_queue.get_nowait())
-            except queue.Empty:
-                break
-        if not ops:
-            return
-        with self._write_lock:
-            with self._write_conn:
-                for query, params in ops:
-                    self._write_conn.execute(query, params)
-
-
-    def _initialize_db(self):
-        # you wouldn't want to do bad things here
-        with open("state_db_init.sql") as f:
-            with self._write_conn:
-                self._write_conn.executescript(f.read())
-
-
-    # now comes business logic
-
-    # startup / batch fetching
-
-    def get_files(self) -> list[dict]:
-        with self._conn:
-            cur = self._conn.execute("SELECT * FROM file")
-            return cur.fetchall()
-
-
-    def get_chunks(self) -> list[dict]:
-        with self._conn:
-            cur = self._conn.execute("SELECT * FROM chunk")
-            return cur.fetchall()
-
-
-    def get_chunks_for_file(self, file_id: str) -> list[dict]:
-        with self._conn:
-            cur = self._conn.execute("SELECT * FROM chunk WHERE file_id = ?", (file_id,))
-            return cur.fetchall()
-
-
-    def get_chunk_worker_status(self, chunk_id: str) -> list[dict]:
-        with self._conn:
-            cur = self._conn.execute("SELECT * FROM worker_status WHERE chunk_id = ?", (chunk_id,))
-            return cur.fetchall()
-
-
-    def get_file_hashes(self):
-        with self._conn:
-            cur = self._conn.execute("SELECT path, md5, sha1, sha256 FROM file_hash JOIN file on file.id = file_hash.file_id")
-            return cur.fetchall()
-
-
-    def get_leaderboard(self):
-        with self._conn:
-            cur = self._conn.execute("SELECT * FROM leaderboard ORDER BY downloaded_bytes DESC")
-            return cur.fetchall()
-
-
-    # file mutations
-
-    def insert_file(self, file_id: str, path: str, size: int, url: str, chunk_size: int, complete: bool = False):
-        self._queue_write(
-            "INSERT INTO file (id, path, size, url, chunk_size, complete) "
-            "VALUES (?, ?, ?, ?, ?, ?)",
-            (file_id, path, size, url, chunk_size, complete)
-        )
-
-
-    def set_file_size(self, file_id: str, size: int):
-        self._queue_write(
-            "UPDATE file SET size = ? WHERE id = ?",
-            (size, file_id)
-        )
-
-
-    def set_file_chunk_size(self, file_id: str, chunk_size: int):
-        self._queue_write(
-            "UPDATE file SET chunk_size = ? WHERE id = ?",
-            (chunk_size, file_id)
-        )
-
-
-    def set_file_complete(self, file_id: str):
-        self._queue_write(
-            "UPDATE file SET complete = 1 WHERE id = ?",
-            (file_id,)
-        )
-
-    # chunk / worker mutations
-
-
-    def insert_chunk(self, chunk_id: str, file_id: str, start: int, end: int):
-        self._queue_write(
-            "INSERT INTO chunk (id, file_id, start, end) "
-            "VALUES (?, ?, ?, ?)",
-            (chunk_id, file_id, start, end)
-        )
-
-
-    def delete_chunk(self, chunk_id: str):
-        self._queue_write(
-            "DELETE FROM chunk WHERE id = ?",
-            (chunk_id,)
-        )
-
-
-    def insert_worker_status(self, chunk_id: str, worker_id: str, uploaded: int = 0, hash: str = "", hash_only: bool = True):
-        self._queue_write(
-            "INSERT OR REPLACE INTO worker_status (chunk_id, worker_id, uploaded, hash, hash_only) "
-            "VALUES (?, ?, ?, ?, ?)",
-            (chunk_id, worker_id, uploaded, hash, hash_only)
-        )
-
-
-    def delete_worker_status(self, chunk_id: str, worker_id: str):
-        self._queue_write(
-            "DELETE FROM worker_status WHERE chunk_id = ? AND worker_id = ?",
+    async def delete_worker_status(self, chunk_id: str, worker_id: str):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+            "DELETE FROM worker_status WHERE chunk_id = $1 AND worker_id = $2",
             (chunk_id, worker_id)
         )
-    
 
-    def delete_chunk_worker_status(self, chunk_id: str):
-        self._queue_write(
-            "DELETE FROM worker_status WHERE chunk_id = ?",
-            (chunk_id,)
-        )
+    async def delete_chunk_worker_status(self, chunk_id: str):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "DELETE FROM worker_status WHERE chunk_id = $1",
+                (chunk_id,)
+            )
+
+    # Worker status mutations
+    async def set_worker_status_uploaded(self, chunk_id: str, worker_id: str, uploaded: int):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE worker_status SET uploaded = $1 WHERE chunk_id = $2 AND worker_id = $3",
+                (uploaded, chunk_id, worker_id)
+            )
+
+    async def set_worker_status_hash(self, chunk_id: str, worker_id: str, hash: str):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE worker_status SET hash = $1 WHERE chunk_id = $2 AND worker_id = $3",
+                (hash, chunk_id, worker_id)
+            )
+
+    async def set_worker_status_hash_only(self, chunk_id: str, worker_id: str, hash_only: bool):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE worker_status SET hash_only = $1 WHERE chunk_id = $2 AND worker_id = $3",
+                (hash_only, chunk_id, worker_id)
+            )
 
 
-    def set_worker_status_uploaded(self, chunk_id: str, worker_id: str, uploaded: int):
-        self._queue_write(
-            "UPDATE worker_status SET uploaded = ? WHERE chunk_id = ? AND worker_id = ?",
-            (uploaded, chunk_id, worker_id)
-        )
-
-
-    def set_worker_status_hash(self, chunk_id: str, worker_id: str, hash: str):
-        self._queue_write(
-            "UPDATE worker_status SET hash = ? WHERE chunk_id = ? AND worker_id = ?",
-            (hash, chunk_id, worker_id)
-        )
-
-
-    def set_worker_status_hash_only(self, chunk_id: str, worker_id: str, hash_only: bool):
-        self._queue_write(
-            "UPDATE worker_status SET hash_only = ? WHERE chunk_id = ? AND worker_id = ?",
-            (hash_only, chunk_id, worker_id)
-        )
-
-
-    # file hash mutations
-
-    def insert_file_hash(self, file_id: str, md5: str, sha1: str, sha256: str):
-        self._queue_write(
-            "INSERT INTO file_hash (file_id, md5, sha1, sha256) "
-            "VALUES (?, ?, ?, ?)",
-            (file_id, md5, sha1, sha256)
-        )
+    # File hash mutations
+    async def insert_file_hash(self, file_id: str, md5: str, sha1: str, sha256: str):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "INSERT INTO file_hash (file_id, md5, sha1, sha256) "
+                "VALUES ($1, $2, $3, $4)"
+                "ON CONFLICT DO UPDATE SET file_id=$1, md5=$2, sha1=$3, sha256=$4",
+                (file_id, md5, sha1, sha256)
+            )
 
     # leaderboard mutations
 
-    def insert_leaderboard_entry(self,
+    async def insert_leaderboard_entry(self,
                                  discord_id: str,
                                  discord_username: str,
                                  avatar_url: str,
                                  downloaded_chunks: int = 0,
                                  downloaded_bytes: int = 0):
-        self._queue_write(
-            "INSERT INTO leaderboard (discord_id, discord_username, avatar_url, downloaded_chunks, downloaded_bytes) "
-            "VALUES (?, ?, ?, ?, ?) "
-            "ON CONFLICT (discord_id) DO NOTHING",
-            (discord_id, discord_username, avatar_url, downloaded_chunks, downloaded_bytes)
-        )
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "INSERT INTO leaderboard (discord_id, discord_username, avatar_url, downloaded_chunks, downloaded_bytes) "
+                "VALUES ($1, $2, $3, $4, $5) "
+                "ON CONFLICT (discord_id) DO NOTHING",
+                (discord_id, discord_username, avatar_url, downloaded_chunks, downloaded_bytes)
+            )
 
-    def update_leaderboard_downloaded_bytes(self, discord_id: str, change: int):
-        self._queue_write(
-            "UPDATE leaderboard SET downloaded_bytes = downloaded_bytes + ? WHERE discord_id = ?",
-            (change, discord_id)
-        )
+    async def update_leaderboard_downloaded_bytes(self, discord_id: str, change: int):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE leaderboard SET downloaded_bytes = downloaded_bytes + $1 WHERE discord_id = $2",
+                (change, discord_id)
+            )
 
-    def update_leaderboard_downloaded_chunks(self, discord_id: str, change: int):
-        self._queue_write(
-            "UPDATE leaderboard SET downloaded_chunks = downloaded_chunks + ? WHERE discord_id = ?",
-            (change, discord_id)
-        )
-
-
-# global singleton and we'll just use the hardcoded filename here
-# each thread will have its own conn on first use
-db: StateDB = StateDB("state.db")
+    async def update_leaderboard_downloaded_chunks(self, discord_id: str, change: int):
+        async with self.connection.cursor() as cur:
+            await cur.execute(
+                "UPDATE leaderboard SET downloaded_chunks = downloaded_chunks + $1 WHERE discord_id = $2",
+                (change, discord_id)
+            )
